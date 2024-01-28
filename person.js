1afd36345b80282a7e5e3b64df44334f36a79b71
Aditi Kumari <aditi712006@gmail.com>
2024-01-28 20:44:26 +0530

    task done

---
+++ person.js
@@ -0,0 +1,22 @@
+function getdetails(details){
+    for(i of details){
+        console.log(i);
+    }
+}
+
+let person = [
+    {
+    name : "Aditi Byahut",
+    age : 18,
+    occupation : "engineer"},
+    {
+    name : "Kajal",
+    age : 19,
+    occupation : "clerk"
+    },
+    {
+    name : "Amisha",
+    age : 21,
+    occupation : "manager"
+    }
+]
